70373b9c7263c9263d75f0d853cd955514dda823
hadarbas <30635499+hadarbas@users.noreply.github.com>
2024-12-20 16:15:18 +0200

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Open_Sans } from 'next/font/google'
+
+const openSans = Open_Sans({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'DocIt',
+  description: 'Turn videos into tutorials with DocIt',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en" className={openSans.className}>
+      <body>{children}</body>
+    </html>
+  )
+}
