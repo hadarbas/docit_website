2c8556452a2a83d20b312b0d9c63ebfdf32b91ab
hadarbas <30635499+hadarbas@users.noreply.github.com>
2024-12-22 10:44:40 +0200

    Update EmailForm.tsx

--- app/components/EmailForm.tsx
+++ app/components/EmailForm.tsx
@@ -15,62 +15,62 @@ export function EmailForm({ buttonText, variant = 'default', icon = null }: { bu
     setIsSubmitted(true);
   }
 
-  const handleOpenModal = () => {
-    setIsModalOpen(true);
-  };
+    const handleOpenModal = () => {
+        setIsModalOpen(true);
+    };
 
-  const handleCloseModal = () => {
-      setIsModalOpen(false);
-      setIsSubmitted(false);
-  };
+    const handleCloseModal = () => {
+        setIsModalOpen(false);
+        setIsSubmitted(false);
+    };
 
-  return (
-    <div className="relative">
-        <button
-          onClick={handleOpenModal}
-          className={`
-            ${variant === 'outline'
-                ? "text-[#2979FF] border-[#2979FF] hover:bg-[#90CAF9] hover:text-white border-2"
-                : "bg-[#2979FF] hover:bg-[#1976D2] text-white"
-            }
-            text-base px-4 py-2 rounded-md flex items-center gap-2
-          `}>
-          {icon}
-          {buttonText}
-        </button>
+    return (
+        <div className="relative">
+            <button
+              onClick={handleOpenModal}
+                className={`
+                    ${variant === 'outline'
+                        ? "text-[#2979FF] border-[#2979FF] hover:bg-[#90CAF9] hover:text-white border-2"
+                        : "bg-[#2979FF] hover:bg-[#1976D2] text-white"
+                    }
+                    text-sm px-4 py-2 rounded-md flex items-center gap-2 h-[40px]
+                `}>
+                {icon}
+                {buttonText}
+            </button>
 
-        {isModalOpen && (
-            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 flex items-center justify-center">
-            <div className="relative w-[300px] p-6 bg-white border rounded-md shadow-md">
-                <div className="flex justify-between items-center mb-4">
-                    <h3 className="text-lg font-semibold">{buttonText}</h3>
-                    <button onClick={handleCloseModal} className="text-gray-600 hover:text-gray-800">
-                       X
-                    </button>
+            {isModalOpen && (
+                <div className="fixed inset-0 bg-gray-500 bg-opacity-75 flex items-center justify-center">
+                    <div className="relative w-[300px] p-6 bg-white border rounded-md shadow-md">
+                        <div className="flex justify-between items-center mb-4">
+                            <h3 className="text-lg font-semibold">{buttonText}</h3>
+                            <button onClick={handleCloseModal} className="text-gray-600 hover:text-gray-800">
+                              X
+                            </button>
+                        </div>
+                        {!isSubmitted ? (
+                            <form onSubmit={handleSubmit} className="grid gap-4 py-4">
+                                <div className="grid grid-cols-4 items-center gap-4">
+                                    <input
+                                    id="email"
+                                    type="email"
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    placeholder="Enter your email"
+                                    className="col-span-4 border rounded-md p-2"
+                                    required
+                                    />
+                                </div>
+                                <button type="submit" className="bg-[#2979FF] hover:bg-[#1976D2] text-white rounded-md p-2">
+                                    Submit
+                                </button>
+                            </form>
+                         ) : (
+                            <p className="py-4 text-center text-green-600">Thank you for your interest! We'll be in touch soon.</p>
+                         )}
+                    </div>
                 </div>
-              {!isSubmitted ? (
-                  <form onSubmit={handleSubmit} className="grid gap-4 py-4">
-                  <div className="grid grid-cols-4 items-center gap-4">
-                      <input
-                      id="email"
-                      type="email"
-                      value={email}
-                      onChange={(e) => setEmail(e.target.value)}
-                      placeholder="Enter your email"
-                      className="col-span-4 border rounded-md p-2"
-                      required
-                      />
-                  </div>
-                  <button type="submit" className="bg-[#2979FF] hover:bg-[#1976D2] text-white rounded-md p-2">
-                      Submit
-                  </button>
-                  </form>
-              ) : (
-                  <p className="py-4 text-center text-green-600">Thank you for your interest! We'll be in touch soon.</p>
-              )}
-            </div>
-            </div>
-        )}
-    </div>
-  )
+            )}
+        </div>
+    )
 }
