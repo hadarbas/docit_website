53d7b532bf345d4a43ec8dd80575ef7b6c89e3bd
hadarbas <30635499+hadarbas@users.noreply.github.com>
2024-12-25 22:09:50 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,11 +6,31 @@ const openSans = Open_Sans({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'DocIt',
-  description: 'From Video to Tutorial in Seconds',
+  description: 'Turn videos into tutorials in seconds',
   icons: [
     { rel: 'icon', url: '/favicon.ico' },
     { rel: 'shortcut icon', url: '/favicon.ico' }
   ],
+  openGraph: {
+    title: 'DocIt',
+    description: 'Turn videos into tutorials in seconds',
+    url: 'https://getdocit.com',
+    siteName: 'DocIt',
+    locale: 'en_US',
+    type: 'website',
+    images: [{
+      url: '/og-image.svg',
+      width: 1200,
+      height: 630,
+      alt: 'DocIt - Turn videos into tutorials in seconds'
+    }]
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'DocIt',
+    description: 'Turn videos into tutorials in seconds',
+    images: ['/og-image.svg']
+  }
 }
 
 export default function RootLayout({
