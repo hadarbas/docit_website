cc056d744117869e730507f16fa7a9236e39cd5b
hadarbas <30635499+hadarbas@users.noreply.github.com>
2025-02-23 10:15:45 +0200

    Add Slack button to Hero section

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -76,6 +76,12 @@ export default function Hero() {
                 </svg>
             </div>
             </div>
+            {/* Add to Slack Button */}
+            <div className="mt-4 flex justify-center">
+                <a href="https://slack.com/oauth/v2/authorize?client_id=YOUR_CLIENT_ID&scope=app_mentions:read,chat:write,commands,files:read,files:write,im:history,users:read,users:read.email&user_scope=&redirect_uri=https://api.getdocit.com/slack/oauth/callback">
+                    <img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" />
+                </a>
+            </div>
             <div className="absolute top-4 right-4">
             <span className="text-gray-400 cursor-not-allowed font-semibold">Log In</span>
             </div>
